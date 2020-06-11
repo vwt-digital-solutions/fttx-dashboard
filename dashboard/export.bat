f044c4fa72898ce8cdbbee3ba8965a69ab035312
Andre van Turnhout <andre.vanturnhout@viqtordavis.com>
2020-06-11 12:07:48 +0200

    modified gitignore and export DAT-2983

--- dashboard/export.bat
+++ dashboard/export.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-set GOOGLE_APPLICATION_CREDENTIALS=C:/Users/agvanturnhout/Documents/ProjectenVWT/Git_repos/fttx-dashboard-vqd-analysis/data_prep/jsonkeys/vwt-d-gew1-fttx-dashboard-6d18934bcfb0.json
+set GOOGLE_APPLICATION_CREDENTIALS=C:/Users/agvanturnhout/Documents/ProjectenVWT/Git_repos/fttx-dashboard-vqd-analysis/jsonkey/vwt-d-gew1-fttx-dashboard-0fdfa671b6da.json
 set OAUTHLIB_INSECURE_TRANSPORT=1
