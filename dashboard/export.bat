0d643e8bf687171d027e21ec27b1e2fd6a4d0379
Andre van Turnhout <andre.vanturnhout@viqtordavis.com>
2020-06-12 16:29:29 +0200

    removed export.bat file and added it to gitignore DAT-3208

--- dashboard/export.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-set GOOGLE_APPLICATION_CREDENTIALS=C:/Users/agvanturnhout/Documents/ProjectenVWT/Git_repos/fttx-dashboard-vqd-analysis/jsonkey/vwt-d-gew1-fttx-dashboard-0fdfa671b6da.json
-set OAUTHLIB_INSECURE_TRANSPORT=1
