4a0589491f223ce5e7a14dea366c6e378e824998
Casper van Houten <c.vanhouten@Caspers-MacBook-Pro.local>
2020-07-31 17:39:05 +0200

    DAN-117 pie charts for reden na. Includes two pie charts, one on main page, one on click of a specific project

--- dashboard/analyse_dashboard/analyse/main.py
+++ dashboard/analyse_dashboard/analyse/main.py
@@ -9,6 +9,7 @@ from functions import get_data_FC, get_data_planning, get_data_targets
 from functions import targets, prognose, overview, calculate_projectspecs, calculate_y_voorraad_act
 from functions import set_filters, prognose_graph, performance_matrix, info_table, set_bar_names, error_check_FCBC
 from functions import graph_overview, masks_phases, map_redenen, analyse_to_firestore, set_date_update
+from functions import overview_reden_na, individual_reden_na
 
 
 logging.basicConfig(level=logging.INFO)
@@ -59,6 +60,9 @@ def analyse(request):
         y_target_l, t_diff = targets(x_prog, x_d, t_shift, date_FTU0, date_FTU1, rc1, d_real_l)
         df_prog, df_target, df_real, df_plan = overview(x_d, y_prog_l, tot_l, d_real_l, HP, y_target_l)
         n_err, errors_FC_BC = error_check_FCBC(df_l)
+        overview_reden_na(df_l)
+        individual_reden_na(df_l)
+
         # write analysis result to Graphs collection
         analyse_to_firestore(date_FTU0, date_FTU1, y_target_l, rc1, x_prog, x_d, d_real_l, df_prog, df_target,
                              df_real, df_plan, HC_HPend, y_prog_l, tot_l, HP, t_shift, rc2, cutoff, y_voorraad_act,
