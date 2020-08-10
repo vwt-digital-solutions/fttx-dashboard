67581123b407f0fa2cc7d3df6f5c48e52ef7bcf0
Erik van Egmond <e.vanegmond@vwt.digital>
2020-08-10 11:35:51 +0200

    DAN-259 Correct import for default layout

--- dashboard/index.py
+++ dashboard/index.py
@@ -1,7 +1,7 @@
 import dash_core_components as dcc
 import dash_html_components as html
 
-import default
+from layout import default
 from app import app
 from dash.dependencies import Input, Output
 from config_pages import config_pages
