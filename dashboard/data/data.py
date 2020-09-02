d8d5229e03f942b91a922a8f320e7e0dc01fdae9
Nik de Geus <>
2020-09-02 07:38:46 +0200

    DAN-300 change columns for bar charts

--- dashboard/data/data.py
+++ dashboard/data/data.py
@@ -5,8 +5,8 @@ import pandas as pd
 
 
 def has_planning_by_week(days_before=30, days_after=30):
-    has_done = collection.get_document(collection="Data", id="count_hasdatum_by_week")
-    has_planning = collection.get_document(collection="Data", id="count_plandatum_by_week")
+    has_done = collection.get_document(collection="Data", id="count_opleverdatum_by_week")
+    has_planning = collection.get_document(collection="Data", id="count_hasdatum_by_week")
     df = pd.DataFrame({**has_planning, **has_done}).reset_index().fillna(0).rename(columns={"index": "date"})
     df['date'] = pd.to_datetime(df['date'], format="%Y-%m-%d")
     start_date = datetime.now() - timedelta(days=days_before)
@@ -16,8 +16,8 @@ def has_planning_by_week(days_before=30, days_after=30):
 
 
 def has_planning_by_month():
-    has_done = collection.get_document(collection="Data", id="count_hasdatum_by_month")
-    has_planning = collection.get_document(collection="Data", id="count_plandatum_by_month")
+    has_done = collection.get_document(collection="Data", id="count_opleverdatum_by_month")
+    has_planning = collection.get_document(collection="Data", id="count_hasdatum_by_month")
     df = pd.DataFrame({**has_planning, **has_done}).reset_index().fillna(0).rename(columns={"index": "date"})
     df['date'] = pd.to_datetime(df['date'], format="%Y-%m-%d")
     start_date = pd.to_datetime(str(datetime.now().year) + '-01-01', format="%Y-%m-%d")
