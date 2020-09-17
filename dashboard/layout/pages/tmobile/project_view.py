e10f0798e37c3f66050b51ad80346c6b47cf6b44
Mark Bruisten <markbruisten@Marks-MacBook-Pro.local>
2020-09-17 17:05:39 +0200

    DAN-342: pie_chart per project on t-mobile site

--- dashboard/layout/pages/tmobile/project_view.py
+++ dashboard/layout/pages/tmobile/project_view.py
@@ -2,10 +2,17 @@ import dash_html_components as html
 
 from data.data import completed_status_counts
 from layout.components.graphs import completed_status_counts_bar
+from data.graph import pie_chart
+from layout.components.figure import figure
 
 
 def get_html(project_name):
     status_counts = completed_status_counts(project_name)
+
+    if project_name:
+        pie_chart_project = pie_chart(client='t-mobile', key=project_name)
+    else:
+        pie_chart_project = {'data': None, 'layout': None}
 
     return [
         html.Div(
@@ -15,6 +22,10 @@ def get_html(project_name):
                                                      title="Status oplevering per fase (LB)"),
                 completed_status_counts_bar.get_html(status_counts.hoogbouw,
                                                      title="Status oplevering per fase (HB & Duplex)"),
+                figure(container_id="pie_chart_project_t-mobile_container",
+                       graph_id="pie_chart_project_t-mobile_container",
+                       className="pretty_container column",
+                       figure=pie_chart_project)
             ]
         )
     ]
