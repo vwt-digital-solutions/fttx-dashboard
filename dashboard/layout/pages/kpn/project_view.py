e7aee1f256bd2a2e42935cbe4bb210f232201978
Erik van Egmond <e.vanegmond@vwt.digital>
2020-10-13 12:02:09 +0200

    DAN-504 Link barchart to piechart on kpn project view

--- dashboard/layout/pages/kpn/project_view.py
+++ dashboard/layout/pages/kpn/project_view.py
@@ -2,11 +2,15 @@ import dash_html_components as html
 
 import dash_core_components as dcc
 import config
+from data.data import no_graph
+from layout.components.figure import figure
+
 colors = config.colors_vwt
 
 
 def get_html(client):
     return [
+        dcc.Store(id=f"status-count-filter-{client}"),
         html.Div(
             id=f'indicators-{client}',
             className="container-display",
@@ -22,21 +26,15 @@ def get_html(client):
                     id='graph_prog_c',
                     className="pretty_container column",
                 ),
-                html.Div(
-                    [dcc.Graph(id="Bar_LB")],
-                    id='Bar_LB_c',
-                    className="pretty_container column",
-                ),
-                html.Div(
-                    [dcc.Graph(id="Bar_HB")],
-                    id='Bar_HB_c',
-                    className="pretty_container column",
-                ),
-                html.Div(
-                    [dcc.Graph(id="Pie_NA_c")],
-                    id='Pie_NA_cid',
-                    className="pretty_container column",
-                ),
+                figure(figure=no_graph(title="Status oplevering per fase (LB)", text='Loading...'),
+                       container_id=f"status-counts-laagbouw-{client}-container",
+                       graph_id=f"status-counts-laagbouw-{client}"),
+                figure(figure=no_graph(title="Status oplevering per fase (HB)", text='Loading...'),
+                       container_id=f"status-counts-hoogbouw-{client}-container",
+                       graph_id=f"status-counts-hoogbouw-{client}"),
+                figure(container_id=f"redenna_project_{client}_container",
+                       graph_id=f"redenna_project_{client}",
+                       figure=no_graph(title="Opgegeven reden na", text='Loading...'))
             ],
             id="main_graphs",
             className="container-display",
