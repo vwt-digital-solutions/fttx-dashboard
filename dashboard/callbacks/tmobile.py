5bab1f589932f31b37b76079c5c7003c7e2f79e1
Nik de Geus <>
2020-11-04 10:07:49 +0100

    DAN-661 resolve merge conflict

--- dashboard/callbacks/tmobile.py
+++ dashboard/callbacks/tmobile.py
@@ -1,6 +1,5 @@
 import dash
 import dash_bootstrap_components as dbc
-import dash_html_components as html
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 from google.cloud import firestore
@@ -89,10 +88,10 @@ def update_indicators(dropdown_selection):
                 ),
                 dbc.ModalFooter(
                     children=[
-                        html.A(
-                            dbc.Button("Download", id="download-indicator", className="ml-auto"),
-                            id=f"indicator-download-{client}",
-                            href="/dash/urlToDownload"),
+                        # html.A(
+                        #     dbc.Button("Download", id="download-indicator", className="ml-auto"),
+                        #     id=f"indicator-download-{client}",
+                        #     href="/dash/urlToDownload"),
                         dbc.Button("Close", id="close-sm", className="ml-auto"),
                     ]
                 ),
