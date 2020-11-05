98f7a9414506015a20d29c52e21595da61e518df
Erik van Egmond <e.vanegmond@vwt.digital>
2020-11-05 10:57:00 +0100

    DAN-662 fix bug

--- dashboard/analyse_dashboard/analyse/Analyse/TMobile.py
+++ dashboard/analyse_dashboard/analyse/Analyse/TMobile.py
@@ -6,6 +6,7 @@ from functions import calculate_projectindicators_tmobile
 from functions_tmobile import calculate_voorraadvormend, add_weeknumber, preprocess_for_jaaroverzicht
 from functions_tmobile import counts_by_time_period, calculate_jaaroverzicht
 from functions import calculate_on_time_ratio, calculate_oplevertijd, calculate_bis_gereed
+from functions import wait_bins
 import logging
 logger = logging.getLogger('T-mobile Analyse')
 
@@ -20,6 +21,7 @@ class TMobileTransform(FttXTransform):
         self._georderd()
         self._opgeleverd()
         self._calculate_oplevertijd()
+        self._waiting_category()
 
     def _georderd(self):
         # Iedere woning met een toestemmingsdatum is geordered door T-mobile.
@@ -35,6 +37,12 @@ class TMobileTransform(FttXTransform):
 
     def _HAS_add_weeknumber(self):
         self.transformed_data.df['has_week'] = add_weeknumber(self.transformed_data.df['hasdatum'])
+
+    def _waiting_category(self):
+        toestemming_df = wait_bins(self.transformed_data.df)
+        toestemming_df_prev = wait_bins(self.transformed_data.df, time_delta_days=7)
+        self.transformed_data.df['wait_category'] = toestemming_df.bins
+        self.transformed_data.df['wait_category_minus_delta'] = toestemming_df_prev.bins
 
 
 class TMobileAnalyse(FttXAnalyse):
