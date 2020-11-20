171b1d008d39be92bc957480811cde6acd06689a
Mark Bruisten <markbruisten@Marks-MacBook-Pro.local>
2020-11-20 17:07:23 +0100

    DAN-756: validator for uploading project rapportage

--- dashboard/upload/Validators.py
+++ dashboard/upload/Validators.py
@@ -1,6 +1,7 @@
 import base64
 import io
 import logging
+import re
 
 import pandas as pd
 from xlrd import XLRDError
@@ -40,8 +41,7 @@ class XLSValidator(Validator):
                 "aangetroffen."
             )
         try:
-
-            self.parsed_file = pd.read_excel(io.BytesIO(self.file_content))
+            self.parsed_file = pd.ExcelFile(io.BytesIO(self.file_content))
             return super().validate()
         except ValidationError as e:
             raise e
@@ -53,13 +53,15 @@ class XLSValidator(Validator):
 
 
 class XLSColumnValidator(XLSValidator):
-    def __init__(self, columns, *args, **kwargs):
+    def __init__(self, columns, sheets=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.parsed_file = None
         self.columns = columns
+        self.sheets = sheets
 
     def validate(self):
         if super().validate():
+            self.parsed_file = self.parsed_file.parse()
             if set(self.parsed_file.columns) != set(self.columns):
                 unexpected = set(self.parsed_file.columns) - set(self.columns)
                 missing = set(self.columns) - set(self.parsed_file.columns)
@@ -69,4 +71,35 @@ class XLSColumnValidator(XLSValidator):
                 if missing:
                     error_message += f"\nNiet aangetroffen in het bestand: {missing}"
                 raise ValidationError(error_message)
+            return True
+
+
+class XLSSheetValidator(XLSValidator):
+    def __init__(self, sheets, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.parsed_file = None
+        self.sheets = sheets
+
+    def validate(self):
+        if super().validate():
+            if not set(self.sheets).issubset(set(self.parsed_file.sheet_names)):
+                missing = set(self.sheets) - set(self.parsed_file.sheet_names)
+                error_message = "De sheets in het bestand zijn niet juist."
+                if missing:
+                    error_message += f"\nNiet aangetroffen in het bestand: {missing}"
+                raise ValidationError(error_message)
+            return True
+
+
+class FileNameValidator(XLSSheetValidator):
+    def __init__(self, patterns, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.patterns = patterns
+
+    def validate(self):
+        if super().validate():
+            for pattern in self.patterns:
+                match = re.findall(list(pattern.values())[0], self.file_name)
+                if not match:
+                    raise ValidationError(f"Ontbreekt in de filenaam: {list(pattern.keys())[0]}")
             return True
