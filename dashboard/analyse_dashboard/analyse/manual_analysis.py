89db573f7947c923b2129ce181ad484d288357ee
avanturnhout <a.vanturnhout@vwt.digital>
2020-11-20 11:49:44 +0100

    added manual analysis function DAN-760

--- dashboard/analyse_dashboard/analyse/manual_analysis.py
+++ dashboard/analyse_dashboard/analyse/manual_analysis.py
@@ -1,9 +1,19 @@
-# make sure you have an updated credential file for fttx production, checked out master and press play
+# before running this script make sure that:
+# 1) you have an updated credential file for fttx production
+# 2) checked out master
+# Then press play :)
+
 import os
 import config
+import time
 from Analyse.KPNDFN import KPNETL
 from Analyse.TMobile import TMobileETL
 from Analyse.KPNDFN import DFNETL
+import logging
+
+logging.basicConfig(
+    format=' %(asctime)s - %(name)s -%(levelname)s -%(filename)s:%(funcName)s:%(lineno)s - %(message)s',
+    level=logging.INFO)
 
 config.path_data_b = 'gs://vwt-p-gew1-fttx-dashboard-stg/'
 config.client_config['kpn']['planning_location'] = \
@@ -21,12 +31,12 @@ config.client_config['dfn']['target_location'] = \
 
 keys = os.listdir(config.path_jsons)
 for fn in keys:
-    if 'd-gew1-fttx' in fn:
+    if 'p-gew1-fttx' in fn:
         os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = config.path_jsons + fn
 
+start = time.time()
 client_name = "kpn"
 kpn = KPNETL(client=client_name, config=config.client_config[client_name])
-kpn.extract()
 kpn.perform()
 
 client_name = "tmobile"
@@ -36,3 +46,4 @@ tmobile.perform()
 client_name = "dfn"
 dfn = DFNETL(client=client_name, config=config.client_config[client_name])
 dfn.perform()
+print(str((time.time() - start) / 60) + ' min')
