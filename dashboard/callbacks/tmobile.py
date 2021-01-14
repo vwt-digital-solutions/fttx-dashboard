de85544a678de297d5e5c072d46bec3ee7c6704e
m.bruisten <m.bruisten@vwt.digital>
2021-01-14 17:04:13 +0100

    DAN-948: removed commented code to clean up

--- dashboard/callbacks/tmobile.py
+++ dashboard/callbacks/tmobile.py
@@ -123,14 +123,6 @@ def FTU_table_editable(ww):
 # update firestore given edit FTU table
 @app.callback(
     [
-        # Output('info_globaal_container0_text', 'children'),
-        # Output('info_globaal_container1_text', 'children'),
-        # Output('info_globaal_container2_text', 'children'),
-        # Output('info_globaal_container3_text', 'children'),
-        # Output('info_globaal_container4_text', 'children'),
-        # Output('info_globaal_container5_text', 'children'),
-        # Output('graph_targets_M', 'figure'),
-        # Output('graph_targets_W', 'figure'),
         Output(f'project-performance-{client}', 'figure'),
     ],
     [
@@ -149,63 +141,5 @@ def FTU_update(data):
     record['record']['FTU1'] = FTU1
     print(record)
     firestore.Client().collection('Data').document(f'{client}_project_dates').set(record)
-
-    # # to update overview graphs:
-    # doc = firestore.Client().collection('Data').document('analysis2').get().to_dict()
-    # doc2 = firestore.Client().collection('Data').document('analysis3').get().to_dict()
-    # x_d = pd.to_datetime(doc['x_d'])
-    # tot_l = doc['tot_l']
-    # HP = doc['HP']
-    # HC_HPend_l = doc['HC_HPend_l']
-    # Schouw_BIS = doc['Schouw_BIS']
-    # # HAS_werkvoorraad = doc['HAS_werkvoorraad']
-    # HPend_l = doc['HPend_l']
-    # d_real_l = doc2['d_real_l']
-    # d_real_li = doc2['d_real_li']
-    # y_prog_l = doc['y_prog_l']
-    # y_target_l = doc['y_target_l']
-    # rc1 = doc['rc1']
-    # rc2 = doc['rc2']
-    # t_shift = doc['t_shift']
-    # cutoff = doc['cutoff']
-    # y_voorraad_act = doc['y_voorraad_act']
-    # x_prog = np.array(doc['x_prog'])
-    # n_err = doc2['n_err']
-    # for key in y_prog_l:
-    #     y_prog_l[key] = np.array(y_prog_l[key])
-    #     y_target_l[key] = np.array(y_target_l[key])
-    #     t_shift[key] = int(t_shift[key])
-    #     if key in rc1:
-    #         rc1[key] = np.array(rc1[key])
-    #     if key in rc2:
-    #         rc2[key] = np.array(rc2[key])
-    #     if key in d_real_l:
-    #         d_real_l[key] = pd.DataFrame(columns=['Aantal'], index=d_real_li[key], data=d_real_l[key])
-    # y_prog_l, _ = update_y_prog_l(FTU0, d_real_l, t_shift, rc1, rc2, y_prog_l, x_d, x_prog, cutoff)
-    # y_target_l, t_diff = targets(x_prog, x_d, t_shift, FTU0, FTU1, rc1, d_real_l)
-    # df_prog, df_target, df_real, df_plan = overview(x_d, y_prog_l, tot_l, d_real_l, HP, y_target_l)
-
-    # # record, data_pr_w, data_t_w, data_r_w, data_p_w = graph_overview(
-    #   df_prog, df_target, df_real, df_plan, HC_HPend, HAS_werkvoorraad, res='W-MON')  # 2019-12-30 -- 2020-12-21
-    # record, jaaroverzicht, data_pr_m, data_t_m, data_r_m, data_p_m = graph_overview(
-    #   df_prog, df_target, df_real, df_plan, HC_HPend, HAS_werkvoorraad, res='M')  # 2019-12-30 -- 2020-12-21
-    # # record = performance_matrix(x_d, y_target_l, d_real_l, tot_l, t_diff, y_voorraad_act)
-    # # record_dict = prognose_graph(x_d, y_prog_l, d_real_l, y_target_l)
-    # # record = info_table(tot_l, d_real_l, HP, y_target_l, x_d, HC_HPend_l, Schouw_BIS, HPend_l, n_err)
-
-    # HC_HPend = jaaroverzicht['HC_HPend']
-    # HAS_werkvoorraad = jaaroverzicht["HAS_werkvoorraad"]
-
-    # out0 = 'HPend afgesproken: ' + jaaroverzicht['target']
-    # out1 = 'HPend gerealiseerd: ' + jaaroverzicht['real']
-    # out2 = 'HPend gepland vanaf nu: ' + jaaroverzicht['plan']
-    # out3 = 'HPend voorspeld vanaf nu: ' + jaaroverzicht['prog']
-    # out4 = jaaroverzicht['HC_HPend']
-    # out5 = HAS_werkvoorraad
-
-    # out6 = collection.get_graph(client="kpn", graph_name='graph_targets_M')
-    # out7 = collection.get_graph(client="kpn", graph_name='graph_targets_W')
-    out8 = collection.get_graph(client=client, graph_name='project_performance')
-
-    # return [out0, out1, out2, out3, out4, out5, out6, out7, out8]
-    return [out8]
+    output = collection.get_graph(client=client, graph_name='project_performance')
+    return [output]
