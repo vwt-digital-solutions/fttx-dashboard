4cc8dfdf045722926044a9ba82cafbc21582623e
Erik van Egmond <e.vanegmond@vwt.digital>
2021-01-18 08:46:58 +0100

    DAN-1009 Added documentation to most of the business rules

--- dashboard/analyse_dashboard/analyse/business_rules.py
+++ dashboard/analyse_dashboard/analyse/business_rules.py
@@ -22,17 +22,25 @@ opleverstatussen = [
     "90",
     "91"
 ]
+"""
+opleverstatussen is a list that contains all possible opleverstatussen.
+"""
 
 
 def is_date_set(series: pd.Series, time_delta_days: int = 0) -> pd.Series:
     """
-    To determine if a date is set.
+    A function that creates a mask for the supplied series that lists which dates are before today's date minus the
+    time_delta_days.
 
-    This function determines if a dates is set at a particular date. By default it checks if it is set for today or
-    earlier. A date must be known (not isna), and it must be before or on the checked date (today minus time_delta_days)
-    :param series:
-    :param time_delta_days:
-    :return:
+    By default it checks if it is set for today or earlier.
+    A date must be known (not isna), and it must be before or on the checked date (today minus time_delta_days)
+
+    Args:
+        series (pd.Series): A series of dates to check.
+        time_delta_days (int): optional, the number of days before today.
+
+    Returns:
+         pd.Series: A series of truth values.
     """
     time_point: pd.Timestamp = (pd.Timestamp.today() - pd.Timedelta(days=time_delta_days))
     return (
@@ -44,6 +52,17 @@ def is_date_set(series: pd.Series, time_delta_days: int = 0) -> pd.Series:
 
 
 def geschouwed(df, time_delta_days=0):
+    """
+    A house is geschouwed when the schouwdatum is not NA and before or on today - time_delta_days.
+    This function uses :meth:`is_date_set`.
+
+    Args:
+        df (pd.DataFrame): A dataframe containing a schouwdatum column with dates.
+        time_delta_days (int): optional, the number of days before today.
+
+    Returns:
+         pd.Series: A series of truth values.
+    """
     return is_date_set(df.schouwdatum, time_delta_days=time_delta_days)
 
 
@@ -53,7 +72,17 @@ def ordered(df, time_delta_days=0):
 
 
 def on_time_opgeleverd(df):
-    # Used to calculate the homes that have been completed within 8 weeks (56 days)
+    """
+    Used to calculate the homes that have been completed within 8 weeks (56 days) after providing permission.
+
+    Args:
+        df (pd.DataFrame): A dataframe containing an opleverdatum column and toestemming_datum column, both containing
+        dates.
+
+    Returns:
+         pd.Series: A series of truth values.
+
+    """
     # TODO: change toestemming_datum to creation (and check other tmobile business rules)
     return (df['opleverdatum'] - df['toestemming_datum']).dt.days <= 56
 
@@ -91,55 +120,144 @@ def te_laat_openstaand(df):
 
 
 def toestemming_bekend(df):
+    """
+    For a house it is known when permission is given when the `toestemming` column is not NA.
+
+    Args:
+        df (pd.DataFrame): A dataframe containing a toestemming column.
+
+    Returns:
+         pd.Series: A series of truth values.
+    """
     return ~df['toestemming'].isna()
 
 
+# TODO: Tjeerd Pols, remove this function.
 def toestemming_gegeven(df):
     return ~df['toestemming_datum'].isna()
 
 
 def laswerk_ap_gereed(df):
+    """
+    Laswerk AP is done when `laswerkapgereed` is set to 1.
+
+    Args:
+        df (pd.DataFrame): A dataframe containing a laswerkapgereed column containing ones and zeroes.
+
+    Returns:
+         pd.Series: A series of truth values.
+    """
     return df['laswerkapgereed'] == '1'
 
 
 def laswerk_ap_niet_gereed(df):
+    """
+    Laswerk AP is done when `laswerkapgereed` is **not** set to 1.
+
+    Args:
+        df (pd.DataFrame): A dataframe containing a laswerkapgereed column containing ones and zeroes.
+
+    Returns:
+         pd.Series: A series of truth values.
+    """
     return df['laswerkapgereed'] != '1'
 
 
 def laswerk_dp_gereed(df):
+    """
+    Laswerk DP is done when `laswerkdpgereed` is set to 1.
+
+    Args:
+        df (pd.DataFrame): A dataframe containing a laswerkdpgereed column containing ones and zeroes.
+
+    Returns:
+         pd.Series: A series of truth values.
+    """
     return df['laswerkdpgereed'] == '1'
 
 
 def laswerk_dp_niet_gereed(df):
+    """
+    Laswerk DP is done when `laswerkdpgereed` is **not** set to 1.
+
+    Args:
+        df (pd.DataFrame): A dataframe containing a laswerkdpgereed column containing ones and zeroes.
+
+    Returns:
+         pd.Series: A series of truth values.
+    """
     return df['laswerkdpgereed'] != '1'
 
 
 # TODO: remove when removing toggle new_structure_overviews
 def bis_opgeleverd(df):
+    """
+    BIS is done when `opleverstatus` is **not** set to 1.
+
+    Args:
+        df (pd.DataFrame): A dataframe containing a opleverstatus column with `opleverstatussen`.
+
+    Returns:
+         pd.Series: A series of truth values.
+    """
     return df['opleverstatus'] != '0'
 
 
+def bis_niet_opgeleverd(df):
+    """
+    BIS is not done when `opleverstatus` is  set to 0.
+
+    Args:
+        df (pd.DataFrame): A dataframe containing a opleverstatus column with `opleverstatussen`.
+
+    Returns:
+         pd.Series: A series of truth values.
+    """
+    return df['opleverstatus'] == '0'
+
+
+# Todo: Andre van Turnhout: Document/remove this function. It should not be named something_new.
 def bis_opgeleverd_new(df):
     return ~df['opleverstatus'].isin(['0', '90', '99'])
 
 
+# TODO: Andre van Turnhout. You should probably have used the hpend() function. If so: refactor the code that uses this
+#  function, otherwise document this function
 def hpend_opgeleverd(df):
     return ~df['opleverdatum'].isna()
 
 
+# TODO: Tjeerd Pols Document this function. Reuse other business rules when applicable.
+#  Here you need to use the opgeleverd and orderd rules.
 def hpend_opgeleverd_and_ordered(df):
     return (~df['opleverdatum'].isna()) & (~df['ordered'].isna())
 
 
-def bis_niet_opgeleverd(df):
-    return df['opleverstatus'] == '0'
-
-
 def hc_opgeleverd(df):
+    """
+    HC (Homes Connected) is done when `opleverstatus` is  set to 2.
+
+    Args:
+        df (pd.DataFrame): A dataframe containing a opleverstatus column with `opleverstatussen`.
+
+    Returns:
+         pd.Series: A series of truth values.
+    """
     return df['opleverstatus'] == '2'
 
 
 def hp_opgeleverd(df):
+    """
+    HP (Homes Passed) is done when `opleverstatus` is **not** set to 2 and `opleverdatum` is not NA.
+    Status 2: a home is connected.
+
+    Args:
+        df (pd.DataFrame): A dataframe containing a opleverstatus column with `opleverstatussen` and a opleverdatum
+        column with dates.
+
+    Returns:
+         pd.Series: A series of truth values.
+    """
     return (
             (df['opleverstatus'] != '2') &
             (~df['opleverdatum'].isna())
@@ -147,6 +265,16 @@ def hp_opgeleverd(df):
 
 
 def has_ingeplanned(df):
+    """
+    HAS is planned when `opleverdatum` is NA and the `hasdatum` is not NA.
+
+    Args:
+        df (pd.DataFrame): A dataframe containing a opleverdatum column with dates and a hasdatum
+        column with dates.
+
+    Returns:
+         pd.Series: A series of truth values.
+    """
     return (
             df['opleverdatum'].isna() &
             ~df['hasdatum'].isna()
@@ -154,6 +282,16 @@ def has_ingeplanned(df):
 
 
 def has_niet_opgeleverd(df):
+    """
+    HAS is not completed when `opleverdatum` is NA and the `hasdatum` is NA.
+
+    Args:
+        df (pd.DataFrame): A dataframe containing a opleverdatum column with dates and a hasdatum
+        column with dates.
+
+    Returns:
+         pd.Series: A series of truth values.
+    """
     return (
             df['opleverdatum'].isna() &
             # TODO is the hasdatum not the planned date? If so, 'has' can be 'niet opgeleverd' but still be planned.
@@ -162,10 +300,30 @@ def has_niet_opgeleverd(df):
 
 
 def hpend(df, time_delta_days=0):
+    """
+    A home is hpend (any kind of completed) when it is :meth:`opgeleverd`.
+
+    Args:
+        df (pd.DataFrame): A dataframe containing a opleverdatum column with dates.
+
+    Returns:
+         pd.Series: A series of truth values.
+    """
     return opgeleverd(df, time_delta_days)
 
 
 def opgeleverd(df, time_delta_days=0):
+    """
+    A home is completed when `opleverdatum` is not NA and before or on today - time_delta_days.
+    This function uses :meth:`is_date_set`.
+
+    Args:
+        df (pd.DataFrame): A dataframe containing a opleverdatum column with dates.
+        time_delta_days (int): optional, the number of days before today.
+
+    Returns:
+         pd.Series: A series of truth values.
+    """
     return is_date_set(df.opleverdatum, time_delta_days=time_delta_days)
 
 
@@ -196,17 +354,23 @@ def has_werkvoorraad(schouw_df, time_delta_days=0):
     )
 
 
+# TODO: refactor this function name. it should not contain _new
 def has_werkvoorraad_new(df, time_delta_days=0):
     """
     This BR determines the werkvoorraad HAS by checking each row of a DataFrame for:
-        Does the df row have a schouwdatum AND is the schouwdatum earlier than today?
-        Does the df row not have a opleverdatum OR is the opleverdatum later than today?
-        Does the df row have a toestemming_datum?
-        Is the df row opleverstatus not equal to 0, 90 or 99?
 
-    :param df: The transformed dataframe
-    :param time_delta_days: An optional offset to today's date
-    :return: A pd.Series mask
+    - Does the df row have a schouwdatum AND is the schouwdatum earlier than today?
+    - Does the df row not have a opleverdatum OR is the opleverdatum later than today?
+    - Does the df row have a toestemming_datum?
+    - Is the df row opleverstatus not equal to 0, 90 or 99?
+
+    Args:
+        df (pd.DataFrame): A  dataframe containing the following columns: [schouwdatum, opleverdatum,
+         toestemming_datum, opleverstatus]
+        time_delta_days (int): optional, the number of days before today.
+
+    Returns:
+        pd.Series: A series of truth values.
     """
     time_point = (pd.Timestamp.today() - pd.Timedelta(days=time_delta_days))
     return (
@@ -233,12 +397,16 @@ def hpend_year(df, year=None):
 def target_tmobile(df):
     """
     This BR determines the target for tmobile by checking each row of a DataFrame for:
-        Does the df row have a creation (date)?
-        Is the df row status not equal to CANCELLED or TO_BE_CANCELLED?
-        Is the df row type equal to AANLEG?
 
-    :param df: The transformed dataframe
-    :return: A pd.Series mask
+    - Does the df row have a creation (date)?
+    - Is the df row status not equal to CANCELLED or TO_BE_CANCELLED?
+    - Is the df row type equal to AANLEG?
+
+    Args:
+        df (pd.DataFrame): The transformed dataframe
+
+    Returns:
+        pd.Series: A series of truth values.
     """
     return (
             (~df.creation.isna())
