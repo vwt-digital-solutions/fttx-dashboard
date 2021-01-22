7ace493a260951f3778dcd13f43099cd617842e1
m.bruisten <m.bruisten@vwt.digital>
2021-01-22 13:33:28 +0100

    DAN-949: updated toggles.py and removed timeseries

--- dashboard/analyse_dashboard/analyse/toggles.py
+++ dashboard/analyse_dashboard/analyse/toggles.py
@@ -3,7 +3,6 @@ from featuretoggles import TogglesList
 
 class ReleaseToggles(TogglesList):
     download_indicators: bool
-    timeseries: bool
     financial_view: bool
     upload: bool
     fc_sql: bool
