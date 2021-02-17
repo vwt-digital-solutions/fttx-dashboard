32629423c560ce975fe4b551a2eeea67ff9178ee
Tjeerd Pols <t.pols@vwt.digital>
2021-02-17 12:08:55 +0100

    DAN-711: fixed front end to display "n.v.t." when toggle is set to false

--- dashboard/callbacks/fttx.py
+++ dashboard/callbacks/fttx.py
@@ -415,7 +415,7 @@ for client in config.client_config.keys():
                                                               client=client,
                                                               year=year,
                                                               frequency="Y"), '.2f'))
-                     if toggles.leverbetrouwbaarheid else None
+                     if toggles.leverbetrouwbaarheid else "n.v.t."
                      )
             ]
             output = global_info_list(items=parameters_global_info_list,
@@ -507,7 +507,7 @@ for client in config.client_config.keys():
                                                               client=client,
                                                               year=year,
                                                               frequency="Y"), '.2f'))
-                     if toggles.leverbetrouwbaarheid else None
+                     if toggles.leverbetrouwbaarheid else "n.v.t."
                      )
             ]
             output = global_info_list_old(items=parameters_global_info_list,
