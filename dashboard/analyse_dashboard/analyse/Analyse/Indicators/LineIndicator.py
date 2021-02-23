160c303477df74c0aa306e40f2ff6216c7f6af6f
avanturnhout <a.vanturnhout@vwt.digital>
2021-02-23 17:27:11 +0100

    added the InternalTargetIndicator to the backend DAN-1060

--- dashboard/analyse_dashboard/analyse/Analyse/Indicators/LineIndicator.py
+++ dashboard/analyse_dashboard/analyse/Analyse/Indicators/LineIndicator.py
@@ -6,9 +6,10 @@ class LineIndicator(Indicator):
     Barebones indicator class containing standard functionality that every type of Indicator will be able to do.
     """
 
-    def __init__(self, client, dates):
+    def __init__(self, client, dates, totals):
         self.client = client
         self.dates = dates
+        self.totals = totals
 
     def transform_dates_to_line(self):
         raise NotImplementedError
