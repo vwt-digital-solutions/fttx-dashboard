1f076613373ffaa1f1d73866cb3496ea034788bf
avanturnhout <a.vanturnhout@vwt.digital>
2021-02-23 19:09:13 +0100

    first try PrognoseIndicator to the backend DAN-1060

--- dashboard/analyse_dashboard/analyse/Analyse/Indicators/LineIndicator.py
+++ dashboard/analyse_dashboard/analyse/Analyse/Indicators/LineIndicator.py
@@ -6,10 +6,11 @@ class LineIndicator(Indicator):
     Barebones indicator class containing standard functionality that every type of Indicator will be able to do.
     """
 
-    def __init__(self, client, dates, totals):
+    def __init__(self, client, dates, totals, df):
         self.client = client
         self.dates = dates
         self.totals = totals
+        self.df = df
 
     def transform_dates_to_line(self):
         raise NotImplementedError
