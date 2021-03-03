38362f2c9adec29e63c0520bf923f9d960d9c011
m.bruisten <m.bruisten@vwt.digital>
2021-03-03 11:57:04 +0100

    DAN-1196: added functionality to linerecord class

--- dashboard/analyse_dashboard/analyse/Analyse/Record/LineRecord.py
+++ dashboard/analyse_dashboard/analyse/Analyse/Record/LineRecord.py
@@ -1,4 +1,8 @@
 from Analyse.Record.Record import Record
+from functions import get_timestamp_of_period
+from toggles import ReleaseToggles
+
+toggles = ReleaseToggles('toggles.yaml')
 
 
 # TODO: Documentation by Casper van Houten
@@ -15,11 +19,21 @@ class LineRecord(Record):
         phase:
         **kwargs:
     """
-
-    def __init__(self, record, collection, client, graph_name, phase, project, **kwargs):
-        super().__init__(record, collection, client, graph_name, **kwargs)
-        self.phase = phase
-        self.project = project
+    if toggles.transform_line_record:
+        def __init__(self, record, collection, client, graph_name, phase, project, resample_method='sum',
+                     to_be_integrated=True, to_be_normalized=True, percentage=True, **kwargs):
+            self.phase = phase
+            self.project = project
+            self.resample_method = resample_method
+            self.to_be_integrated = to_be_integrated
+            self.to_be_normalized = to_be_normalized
+            self.percentage = percentage
+            super().__init__(record, collection, client, graph_name, **kwargs)
+    else:
+        def __init__(self, record, collection, client, graph_name, phase, project, **kwargs):
+            super().__init__(record, collection, client, graph_name, **kwargs)
+            self.phase = phase
+            self.project = project
 
     def _to_document(self):
         return dict(record=self.record,
@@ -37,27 +51,83 @@ class LineRecord(Record):
         Returns:
             record (dict): dictionary of aggregates required for dashboard.
         """
-        record_to_write = dict()
-        record_to_write['series_week'] = record.get_line_aggregate(freq='W-MON',
-                                                                   loffset='-1',
-                                                                   aggregate_type='series',
-                                                                   index_as_str=True).to_dict()
-        record_to_write['series_month'] = record.get_line_aggregate(freq='MS',
-                                                                    aggregate_type='series',
-                                                                    index_as_str=True).to_dict()
-        if record.name == 'work_stock_amount_indicator':
-            record_to_write['next_week'] = record.get_line_aggregate(freq='W-MON',
-                                                                     loffset='-1',
-                                                                     aggregate_type='value_mean')
-            record_to_write['next_month'] = record.get_line_aggregate(freq='MS',
-                                                                      aggregate_type='value_mean')
+
+        if toggles.transform_line_record:
+            record_to_write = dict()
+            record_to_write['configuration'] = {
+                'resample method': self.resample_method,
+                'integrated': self.to_be_integrated,
+                'normalized': self.to_be_normalized,
+                'percentage': self.percentage
+            }
+            line = record
+            line_week = line.resample(freq='W-MON', method=self.resample_method, loffset='-1')
+            line_month = line.resample(freq='MS', method=self.resample_method, loffset='-1')
+
+            last_week = get_timestamp_of_period(freq='W-MON', period='last_period')
+            record_to_write['last_week'] = line_week.make_series()[
+                last_week] if last_week in line_week.make_series().index else 0
+
+            current_week = get_timestamp_of_period(freq='W-MON', period='current_period')
+            record_to_write['current_week'] = line_week.make_series()[
+                current_week] if current_week in line_week.make_series().index else 0
+
+            next_week = get_timestamp_of_period(freq='W-MON', period='next_period')
+            record_to_write['next_week'] = line_week.make_series()[
+                next_week] if next_week in line_week.make_series().index else 0
+
+            last_month = get_timestamp_of_period(freq='MS', period='last_period')
+            record_to_write['last_month'] = line_month.make_series()[
+                last_month] if last_month in line_month.make_series().index else 0
+
+            current_month = get_timestamp_of_period(freq='MS', period='current_period')
+            record_to_write['current_month'] = line_month.make_series()[
+                current_month] if current_month in line_month.make_series().index else 0
+
+            next_month = get_timestamp_of_period(freq='MS', period='next_period')
+            record_to_write['next_month'] = line_month.make_series()[
+                next_month] if next_month in line_month.make_series().index else 0
+
+            if self.to_be_integrated:
+                line_week = line_week.integrate()
+                line_month = line_month.integrate()
+
+            if self.to_be_normalized:
+                series_week = line_week.make_normalised_series(percentage=self.percentage)
+                series_month = line_month.make_normalised_series(percentage=self.percentage)
+            else:
+                series_week = line_week.make_series()
+                series_month = line_month.make_series()
+
+            series_week.index = series_week.index.format()
+            record_to_write['series_week'] = series_week.to_dict()
+            series_month.index = series_month.index.format()
+            record_to_write['series_month'] = series_month.to_dict()
+
+            return record_to_write
+
         else:
-            record_to_write['next_week'] = record.get_line_aggregate(freq='W-MON',
-                                                                     loffset='-1',
-                                                                     aggregate_type='value_sum')
-            record_to_write['next_month'] = record.get_line_aggregate(freq='MS',
-                                                                      aggregate_type='value_sum')
-        return record_to_write
+            record_to_write = dict()
+            record_to_write['series_week'] = record.get_line_aggregate(freq='W-MON',
+                                                                       loffset='-1',
+                                                                       aggregate_type='series',
+                                                                       index_as_str=True).to_dict()
+            record_to_write['series_month'] = record.get_line_aggregate(freq='MS',
+                                                                        aggregate_type='series',
+                                                                        index_as_str=True).to_dict()
+            if record.name == 'work_stock_amount_indicator':
+                record_to_write['next_week'] = record.get_line_aggregate(freq='W-MON',
+                                                                         loffset='-1',
+                                                                         aggregate_type='value_mean')
+                record_to_write['next_month'] = record.get_line_aggregate(freq='MS',
+                                                                          aggregate_type='value_mean')
+            else:
+                record_to_write['next_week'] = record.get_line_aggregate(freq='W-MON',
+                                                                         loffset='-1',
+                                                                         aggregate_type='value_sum')
+                record_to_write['next_month'] = record.get_line_aggregate(freq='MS',
+                                                                          aggregate_type='value_sum')
+            return record_to_write
 
     def document_name(self, **kwargs):
         """
