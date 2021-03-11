0f754211608015b6049a71e9c0cfc1322ca385cc
avanturnhout <a.vanturnhout@vwt.digital>
2021-03-11 10:20:55 +0100

    refactor InternalTargetIndicator for clarity DAN-1199

--- dashboard/analyse_dashboard/analyse/Analyse/Indicators/InternalTargetIndicator.py
+++ dashboard/analyse_dashboard/analyse/Analyse/Indicators/InternalTargetIndicator.py
@@ -10,45 +10,38 @@ from Analyse.Record.RecordList import RecordList
 class InternalTargetIndicator(LineIndicator):
 
     def perform(self):
-        record_list = RecordList()
-        line_client, total_amount_client = self.initialize_line_client(self.project_info)
+        list_line_records = RecordList()
+        line_client_aggregate = self.initialize_line_client_aggregate()
         for project in self.project_info:
-            line = self.transform_dates_to_line(self.project_info[project])
-            line_client = line_client.add(line, fill_value=0)
-            record_list.append(self.to_record(line, project))
-        line_client.name = 'InternalTargetLine'
-        line_client.max_value = total_amount_client
-        record_list.append(self.to_record(line_client, project='client_aggregate'))
+            line_project = self.make_line_from_project_dates(project)
+            list_line_records.append(self.make_record_from_line(line_project, project))
+            line_client_aggregate = line_client_aggregate.add(line_project, fill_value=0)
+        line_client_aggregate.name = 'InternalTargetLine'
+        list_line_records.append(self.make_record_from_line(line_client_aggregate, project='client_aggregate'))
 
-        return record_list, line_client
+        return list_line_records
 
-    def initialize_line_client(self, project_info):
-        first_FTU0_client = min([dict['FTU0'] for dict in self.project_info.values() if dict['FTU0']])
-        last_FTU1_client = max([dict['FTU1'] for dict in self.project_info.values() if dict['FTU1']])
-        total_amount_client = sum([dict['huisaansluitingen'] for dict in self.project_info.values()
-                                   if dict['huisaansluitingen']])
-        line_client = TimeseriesLine(data=0, domain=DateDomain(begin=first_FTU0_client, end=last_FTU1_client))
+    def initialize_line_client_aggregate(self):
+        first_FTU0_client = min([project['FTU0'] for project in self.project_info.values() if project['FTU0']])
+        last_FTU1_client = max([project['FTU1'] for project in self.project_info.values() if project['FTU1']])
+        domain = DateDomain(begin=first_FTU0_client, end=last_FTU1_client)
+        line = TimeseriesLine(data=0, domain=domain)
+        return line
 
-        return line_client, total_amount_client
-
-    def transform_dates_to_line(self, project_info):
-        if (project_info['FTU0'] is not None) & \
-           (project_info['FTU1'] is not None) & \
-           (project_info['huisaansluitingen'] is not None):
-            slope = project_info['huisaansluitingen'] / (pd.to_datetime(project_info['FTU1']) -
-                                                         pd.to_datetime(project_info['FTU0'])).days
-            domain = DateDomain(begin=project_info['FTU0'],
-                                end=pd.to_datetime(project_info['FTU1']) - timedelta(days=1))
+    def make_line_from_project_dates(self, project):
+        start_project = self.project_info[project]['FTU0']
+        end_project = self.project_info[project]['FTU1']
+        total_amount = self.project_info[project]['huisaansluitingen']
+        if (start_project is not None) & (end_project is not None) & (total_amount is not None):
+            slope = total_amount / (pd.to_datetime(end_project) - pd.to_datetime(start_project)).days
+            domain = DateDomain(begin=start_project, end=pd.to_datetime(end_project) - timedelta(days=1))
         else:
             slope = 0
             domain = DateDomain(begin=pd.Timestamp.now(), end=pd.Timestamp.now())
-        line = TimeseriesLine(data=slope,
-                              domain=domain,
-                              name='InternalTargetLine',
-                              max_value=project_info['huisaansluitingen'])
+        line = TimeseriesLine(data=slope, domain=domain, name='InternalTargetLine', max_value=total_amount)
         return line
 
-    def to_record(self, line, project):
+    def make_record_from_line(self, line, project):
         return LineRecord(record=line,
                           collection='Lines',
                           graph_name=f'{line.name}',
