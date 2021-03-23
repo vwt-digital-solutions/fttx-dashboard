21b15959804337ee6b098ec012307973360b98e0
avanturnhout <a.vanturnhout@vwt.digital>
2021-03-23 11:45:51 +0100

    added internaltargetindicator for Tmobile DAN-1200

--- dashboard/analyse_dashboard/analyse/Analyse/Indicators/HcOpenIndicator.py
+++ dashboard/analyse_dashboard/analyse/Analyse/Indicators/HcOpenIndicator.py
@@ -1,18 +1,30 @@
-from Analyse.Aggregators.Aggregator import Aggregator
-from Analyse.Indicators.ActualIndicator import ActualIndicator
+from Analyse.Indicators.DataIndicator import DataIndicator
 import business_rules as br
+import copy
+from Analyse.Record.Record import Record
 
 
-class HcOpenIndicator(ActualIndicator, Aggregator):
-
-    def __init__(self):
-        """
-        Indicator to calculate current werkvoorraad
-        """
-        super().__init__()
-        self.graph_name = 'HCopen'
-
+class HcHpEndIndicator(DataIndicator):
+    """
+    Indicator to
+    """
     def apply_business_rules(self):
-        df = self.df[br.openstaande_orders_tmobile(self.df)]
-        df = df[['project', 'sleutel']]
+        df = copy.deepcopy(self.df)
+        df['HPend'] = br.hpend(df)
+        df['HC'] = br.hc_opgeleverd(df)
+        df = df[['HC', 'HPend', 'project']]
         return df
+
+    def perform(self):
+        df = self.aggregate(df=self.apply_business_rules(),
+                            by='project',
+                            agg_function='sum')
+        ratio = (df['HC'] / df['HPend']).fillna(0)
+        return self.to_record(ratio)
+
+    def to_record(self, series):
+        record = series.to_dict()
+        return Record(record=record,
+                      collection='Data',
+                      client=self.client,
+                      graph_name='ratio_hc_hpend')
