52f75e8214d20a019dd8c4d193673b6f97a56ba2
avanturnhout <a.vanturnhout@vwt.digital>
2021-03-25 09:53:38 +0100

    corrected br for realisationHPcivielindicator DAN-1201

--- dashboard/analyse_dashboard/analyse/Analyse/Indicators/RealisationHPcivielIndicator.py
+++ dashboard/analyse_dashboard/analyse/Analyse/Indicators/RealisationHPcivielIndicator.py
@@ -1,3 +1,6 @@
+import copy
+
+import business_rules as br
 from Analyse.Indicators.RealisationIndicator import RealisationIndicator
 
 
@@ -12,6 +15,19 @@ class RealisationHPcivielIndicator(RealisationIndicator):
         super().__init__(project_info, return_lines, **kwargs)
         self.project_info = project_info
         self.return_lines = return_lines
-        self.type_total_amount = 'huisaansluitingen'
-        self.columns = ['project', 'status_civiel_datum']
-        self.indicator_name = 'RealisationHPcivielIndicator'
+        self.type_total_amount = "huisaansluitingen"
+        self.columns = ["project", "status_civiel_datum"]
+        self.indicator_name = "RealisationHPcivielIndicator"
+
+    def apply_business_rules(self):
+        """
+        HC and HPend columns are needed, as we will calculate ratio between these two columns.
+        Opleverdatum and project columns are used for aggregations.
+
+        Returns: Sliced dataframe with only relevant columns.
+
+        """
+        df = copy.deepcopy(self.df)
+        df = df[br.bis_opgeleverd(df)]
+        df = df[self.columns]
+        return df
