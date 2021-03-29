3f4b57cee3890b55881f635a7098488db66281c7
caspervanhouten <c.vanhouten@vwt.digital>
2021-03-29 09:15:38 +0200

    DAN-1206 Added logging to creation of indicator

--- dashboard/analyse_dashboard/analyse/Analyse/Indicators/Indicator.py
+++ dashboard/analyse_dashboard/analyse/Analyse/Indicators/Indicator.py
@@ -1,9 +1,18 @@
+import logging
 from abc import ABC, abstractmethod
+
+logging.basicConfig(
+    format=" %(asctime)s - %(name)s -%(levelname)s - %(filename)s:%(funcName)s:%(lineno)s - %(message)s",
+    level=logging.INFO,
+)
+
+logger = logging.getLogger("Indicators")
 
 
 class Indicator(ABC):
-
-    def __init__(self, df, client):
+    def __init__(self, df, client, silence=False):
+        if not silence:
+            logger.info(f"Performing {self.__class__.__name__} indicator")
         self.df = df
         self.client = client
 
