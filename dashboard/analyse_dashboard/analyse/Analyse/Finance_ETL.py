7b2b965bc14290fc579b1ef067cf9b5750037a10
m.bruisten <m.bruisten@vwt.digital>
2021-03-30 10:05:23 +0200

    DAN-1231: updated finance_ETL extract

--- dashboard/analyse_dashboard/analyse/Analyse/Finance_ETL.py
+++ dashboard/analyse_dashboard/analyse/Analyse/Finance_ETL.py
@@ -4,31 +4,70 @@ Finance_ETL.py
 
 The ETL process for FttX Finance analyse.
 """
+from sqlalchemy import bindparam, text
+
 from Analyse.ETL import Extract, ETL, Transform, Load, ETLBase, logger
 import pandas as pd
 
 from Analyse.Record.RecordList import RecordList
+from functions import get_database_engine
 
 
 class FinanceExtract(Extract):
-
+    """
+    Extracts data that is necesarry for the financial analyses
+    """
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.projects = self.config["projects"]
+        self.client_name = self.config.get("name")
 
     def extract(self):
+        """
+        Extracts the data for every project for the budget, the realisation and the categorisation to group
+        certain costs.
+        """
         super().extract()
-        self.extract_baan_budget_from_sql()
-        self.extract_baan_realisation_from_sql()
+        self.extracted_data.baan_budget = self.extract_sql_table('baan_budget')
+        self.extracted_data.baan_realisation = self.extract_sql_table('baan_realisation')
         self.extract_categorisering()
 
-    def extract_baan_budget_from_sql(self):
-        self.extracted_data.baan_budget = pd.DataFrame()
+    def extract_sql_table(self, table):
+        """
+        Extracts sql table
+        Args:
+            table (str): table to extract
 
-    def extract_baan_realisation_from_sql(self):
-        self.extracted_data.baan_realisation = pd.DataFrame()
+        Returns: pd.DataFrame containing the data of the table
+
+        """
+
+        logger.info(f"Extracting {table} from the sql database")
+        sql = text(
+            f"""
+            SELECT baan.*, fcm.project_naam
+            FROM {table} baan
+            INNER JOIN fc_baan_project_nr_name_map fcm
+            ON baan.project = fcm.baan_nummer
+            WHERE fcm.project_naam in :projects
+            """  # nosec
+        ).bindparams(bindparam("projects", expanding=True))
+        df = pd.read_sql(sql, get_database_engine(), params={"table": table, "projects": tuple(self.projects)})
+        projects_category = pd.CategoricalDtype(categories=self.projects)
+        df["project"] = df.project.astype(projects_category)
+        return df
 
     def extract_categorisering(self):
-        self.extracted_data.categorisation = pd.DataFrame()
+        """
+        Extracts the categorisation from the sql database
+
+        Returns: pd.DataFrame containing the categorisation
+
+        """
+
+        logger.info("Extracting categorisation from the sql database")
+        df = pd.read_excel(self.config['categorisation'], index=False)
+        self.extracted_data.categorisation = df
 
 
 class FinanceTransform(Transform):
