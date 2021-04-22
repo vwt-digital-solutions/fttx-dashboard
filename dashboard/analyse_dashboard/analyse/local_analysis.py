f2858ba59ab5b222a99419f24f26a079973e4ec1
caspervanhouten <c.vanhouten@vwt.digital>
2021-04-22 09:36:25 +0200

    DAN-1209 Added correct ETL-processes to local_analysis, now uses Indicatoranalyses everywhere

--- dashboard/analyse_dashboard/analyse/local_analysis.py
+++ dashboard/analyse_dashboard/analyse/local_analysis.py
@@ -1,18 +1,24 @@
 # %%
-import logging
 import argparse
+import logging
 import os
 import sys
-
-import config
-from Analyse.KPNDFN import DFNLocalETL, KPNETL, KPNLocalETL, DFNETL, KPNTestETL, DFNTestETL
-from Analyse.TMobile import TMobileLocalETL, TMobileETL, TMobileTestETL
-
 from builtins import input
 
+import config
+from Analyse.KPNIndicatorAnalysis import (DFNIndicatorAnalyse,
+                                          DFNIndicatorLocalETL,
+                                          DFNIndicatorTestETL,
+                                          KPNIndicatorAnalyse,
+                                          KPNIndicatorLocalETL,
+                                          KPNIndicatorTestETL,
+                                          TmobileIndicatorAnalyse,
+                                          TmobileIndicatorLocalETL,
+                                          TmobileIndicatorTestETL)
+
 logging.basicConfig(
-    format=' %(asctime)s - %(name)s -%(levelname)s - %(filename)s:%(funcName)s:%(lineno)s - %(message)s',
-    level=logging.INFO
+    format=" %(asctime)s - %(name)s -%(levelname)s - %(filename)s:%(funcName)s:%(lineno)s - %(message)s",
+    level=logging.INFO,
 )
 
 
@@ -27,15 +33,17 @@ def run_client(client_name, etl_process, steps=None):
     """
     etl = etl_process(client=client_name, config=config.client_config[client_name])
     if steps is None:
-        print(f'Performing {etl_process.__name__} for {client_name}')
+        print(f"Performing {etl_process.__name__} for {client_name}")
         etl.perform()
     else:
         step_list = [etl.extract, etl.transform, etl.analyse, etl.load]
-        print(f"Performing {steps} steps for {etl_process.__name__}, client: {client_name}")
+        print(
+            f"Performing {steps} steps for {etl_process.__name__}, client: {client_name}"
+        )
         [step() for step in step_list[:steps]]
 
 
-def get_etl_process(client, etl_type='local'):
+def get_etl_process(client, etl_type="local"):
     """
     Retrieves ETL process given type and client
     Args:
@@ -46,34 +54,34 @@ def get_etl_process(client, etl_type='local'):
 
     """
     etl_processes = {
-        'kpn': {
-            'local': KPNLocalETL,
-            'write_to_dev': KPNETL,
-            'reload': KPNTestETL
+        "kpn": {
+            "local": KPNIndicatorLocalETL,
+            "write_to_dev": KPNIndicatorAnalyse,
+            "reload": KPNIndicatorTestETL,
         },
-        'tmobile': {
-            'local': TMobileLocalETL,
-            'write_to_dev': TMobileETL,
-            'reload': TMobileTestETL
+        "tmobile": {
+            "local": TmobileIndicatorLocalETL,
+            "write_to_dev": TmobileIndicatorAnalyse,
+            "reload": TmobileIndicatorTestETL,
         },
-        'dfn': {
-            'local': DFNLocalETL,
-            'write_to_dev': DFNETL,
-            'reload': DFNTestETL
-        }
+        "dfn": {
+            "local": DFNIndicatorLocalETL,
+            "write_to_dev": DFNIndicatorAnalyse,
+            "reload": DFNIndicatorTestETL,
+        },
     }
     return etl_processes[client][etl_type]
 
 
 def set_config_project(project, client):
-    if project in config.client_config[client]['projects']:
-        config.client_config[client]['projects'] = [project]
+    if project in config.client_config[client]["projects"]:
+        config.client_config[client]["projects"] = [project]
     else:
         raise ValueError("Please select a valid combination of client and project")
 
 
 def remove_pickle(client):
-    filename = f'{client}_data.pickle'
+    filename = f"{client}_data.pickle"
     if os.path.exists(filename):
         os.remove(filename)
         print("Old pickle has been removed.")
@@ -84,32 +92,40 @@ def remove_pickle(client):
 def force_reload(clients):
     for client in clients:
         remove_pickle(client)
-        extract_process = get_etl_process(client=client, etl_type='reload')
+        extract_process = get_etl_process(client=client, etl_type="reload")
         run_client(client, extract_process, steps=1)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--remote',
-                        help='Write results to actual firestore, otherwise use the local firestore',
-                        action='store_true')
-    parser.add_argument('--client',
-                        choices=['kpn', 'dfn', 'tmobile'],
-                        required='--project' in sys.argv,
-                        help='Run the analysis for a specific client',
-                        )
-    parser.add_argument('--project',
-                        help='Run the analysis only for a specific project. Requires --client input.')
-    parser.add_argument('--force-reload',
-                        action='store_true',
-                        help="Force reloading data from database, rather than using local pickle.")
-    parser.add_argument('--steps',
-                        choices=['1', '2', '3', '4'],
-                        default='4',
-                        help='Run the code only up to a certain point of the process: '
-                             '1.Extract, 2.Transform, 3.Analyse, 4.Load (default = 4)'
-                        )
+    parser.add_argument(
+        "--remote",
+        help="Write results to actual firestore, otherwise use the local firestore",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--client",
+        choices=["kpn", "dfn", "tmobile"],
+        required="--project" in sys.argv,
+        help="Run the analysis for a specific client",
+    )
+    parser.add_argument(
+        "--project",
+        help="Run the analysis only for a specific project. Requires --client input.",
+    )
+    parser.add_argument(
+        "--force-reload",
+        action="store_true",
+        help="Force reloading data from database, rather than using local pickle.",
+    )
+    parser.add_argument(
+        "--steps",
+        choices=["1", "2", "3", "4"],
+        default="4",
+        help="Run the code only up to a certain point of the process: "
+        "1.Extract, 2.Transform, 3.Analyse, 4.Load (default = 4)",
+    )
     args = parser.parse_args()
     remote = args.remote
     project = args.project
@@ -118,11 +134,11 @@ if __name__ == "__main__":
     if client:
         clients = [client]
     else:
-        clients = ['kpn', 'tmobile', 'dfn']
+        clients = ["kpn", "tmobile", "dfn"]
 
     if args.force_reload:
-        if 'FIRESTORE_EMULATOR_HOST' in os.environ:
-            del os.environ['FIRESTORE_EMULATOR_HOST']
+        if "FIRESTORE_EMULATOR_HOST" in os.environ:
+            del os.environ["FIRESTORE_EMULATOR_HOST"]
         force_reload(clients)
 
     steps = int(args.steps)
@@ -133,14 +149,18 @@ if __name__ == "__main__":
         set_config_project(project, client)
 
     if remote:
-        prompt = input("Data will be written to the development firestore, confirm that this is intentional (y/n):")
+        prompt = input(
+            "Data will be written to the development firestore, confirm that this is intentional (y/n):"
+        )
 
-        if prompt != 'y':
-            raise ValueError("Please run without --remote to write to the local firestore.")
-        etl_type = 'write_to_dev'
+        if prompt != "y":
+            raise ValueError(
+                "Please run without --remote to write to the local firestore."
+            )
+        etl_type = "write_to_dev"
     else:
-        os.environ['FIRESTORE_EMULATOR_HOST'] = 'localhost:8080'
-        etl_type = 'local'
+        os.environ["FIRESTORE_EMULATOR_HOST"] = "localhost:8080"
+        etl_type = "local"
 
     for client in clients:
         etl_process = get_etl_process(client=client, etl_type=etl_type)
