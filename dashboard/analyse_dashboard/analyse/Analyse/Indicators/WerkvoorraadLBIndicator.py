07b3622a76ff59ac683d07a2c11542309149f384
caspervanhouten <c.vanhouten@vwt.digital>
2021-06-02 09:45:34 +0200

    DAN-1300 Werkvoorraadindicatoren based on Gerhards bouwportaal data.

---
+++ dashboard/analyse_dashboard/analyse/Analyse/Indicators/WerkvoorraadLBIndicator.py
@@ -0,0 +1,25 @@
+import copy
+
+import business_rules as br
+from Analyse.Indicators.ActualIndicator import ActualIndicator
+
+
+class WerkvoorraadLBIndicator(ActualIndicator):
+    def __init__(self, **kwargs):
+        """
+        Indicator to calculate current werkvoorraad for LB
+        """
+        super().__init__(**kwargs)
+        self.graph_name = "WerkvoorraadLBIndicator"
+
+    def apply_business_rules(self):
+        """
+        Slice all rows that are currently in werkvoorraad, and retrieve
+        the relevant columns to group on.
+
+        Returns: DataFrame with relevant rows and columns.
+
+        """
+        df = copy.deepcopy(self.df)
+        df["werkvoorraad"] = br.is_in_lb_werkvoorraad(df)
+        return df[["project", "werkvoorraad"]]
