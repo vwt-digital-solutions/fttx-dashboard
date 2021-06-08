d92827e6c15986a0c7ae74cf2721f5fcca82e03b
caspervanhouten <c.vanhouten@vwt.digital>
2021-06-08 11:11:03 +0200

    DAN-1301 Afsluitindicator timeseries activatie realisation

---
+++ dashboard/analyse_dashboard/analyse/Analyse/Indicators/AfsluitIndicator.py
@@ -0,0 +1,15 @@
+from Analyse.Indicators.RealisationIndicator import RealisationIndicator
+
+
+class AfsluitIndicator(RealisationIndicator):
+    """
+    Indicator to calculate number of houses realised over days per project.
+    Makes LineRecords per project, where all relevant details can be calculated.
+    Also makes a LineRecords for the aggregate of the project lines for a given client
+    """
+
+    def __init__(self, project_info, return_lines=False, **kwargs):
+        super().__init__(project_info, return_lines, **kwargs)
+        self.type_total_amount = "huisaansluitingen"
+        self.columns = ["project", "afsluitdatum"]
+        self.indicator_name = "AfsluitIndicator"
