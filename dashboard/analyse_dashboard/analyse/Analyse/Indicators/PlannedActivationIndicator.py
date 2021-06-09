215b28576f62ec5266ea41ce5bff2e399ea7e56b
avanturnhout <a.vanturnhout@vwt.digital>
2021-06-09 11:24:16 +0200

    tweaked PlannedActivationIndicator DAN-1201

--- dashboard/analyse_dashboard/analyse/Analyse/Indicators/PlannedActivationIndicator.py
+++ dashboard/analyse_dashboard/analyse/Analyse/Indicators/PlannedActivationIndicator.py
@@ -1,3 +1,6 @@
+import copy
+
+import business_rules as br
 from Analyse.Indicators.RealisationIndicator import RealisationIndicator
 
 
@@ -8,8 +11,21 @@ class PlannedActivationIndicator(RealisationIndicator):
     Also makes a LineRecords for the aggregate of the project lines for a given client
     """
 
-    def __init__(self, project_info, return_lines=False, **kwargs):
+    def __init__(self, project_info=None, return_lines=False, **kwargs):
         super().__init__(project_info, return_lines, **kwargs)
-        self.type_total_amount = "huisaansluitingen"
+        self.type_total_amount = None
         self.columns = ["project", "plandatum"]
         self.indicator_name = "PlannedActivationIndicator"
+
+    def apply_business_rules(self):
+        """
+        HC and HPend columns are needed, as we will calculate ratio between these two columns.
+        Opleverdatum and project columns are used for aggregations.
+
+        Returns: Sliced dataframe with only relevant columns.
+
+        """
+        df = copy.deepcopy(self.df)
+        df = df[br.mask_plandatum_notna(df)]
+        df = df[self.columns]
+        return df
