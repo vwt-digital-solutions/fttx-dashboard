1776cc8ff4a2f9276b4d2c8aeaac55802529e6b7
m.bruisten <m.bruisten@vwt.digital>
2021-06-09 16:49:40 +0200

    DAN-1316: update layout of activatie tab

--- dashboard/layout/components/graphs/horizontal_bar_chart.py
+++ dashboard/layout/components/graphs/horizontal_bar_chart.py
@@ -3,7 +3,7 @@ from plotly import graph_objects as go
 import config
 
 
-def get_fig(*bars):
+def get_fig(bar):
     fig = go.Figure(
         [
             go.Bar(
@@ -11,13 +11,16 @@ def get_fig(*bars):
                 x=bar.get("x"),
                 y=bar.get("y"),
                 marker_color=bar.get('color'),
-                orientation='h'
-            ) for bar in bars
+                orientation='h',
+                text=bar[bar.get('text')] if 'text' in bar else '',
+                textposition='outside'
+            )
         ]
     )
     fig.update_layout(
         height=500,
         paper_bgcolor=config.colors_vwt['paper_bgcolor'],
         plot_bgcolor=config.colors_vwt['plot_bgcolor'],
+        title=bar.get('title') if 'title' in bar else ''
     )
     return fig
